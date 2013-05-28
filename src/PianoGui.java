c32e069bd70f1a941daeb25cf8150dbf22816ec7
Gunther Cox <gunthercx@gmail.com>
2013-05-27 20:03:54 -0400

    built out piano mode

--- src/PianoGui.java
+++ src/PianoGui.java
@@ -14,7 +14,6 @@ public class PianoGui extends JFrame implements KeyListener {
 		
 		setLayout(new BorderLayout());
 		
-		JTextArea prompt = new JTextArea("Enter your name:");
 		JEditorPane text = new JTextPane();
 		
 		//prompt.add(text);
@@ -31,9 +30,162 @@ public class PianoGui extends JFrame implements KeyListener {
 		public void keyPressed(KeyEvent e) {
 
 			int key = e.getKeyCode();
-
-			if (key == KeyEvent.VK_ENTER) {
+			
+			// THIS IS HORRIBLE BECAUSE IT IS COMPLETELY HARDCODED
+			
+			switch (key) {
+			
+			case KeyEvent.VK_ENTER:
+				toneGenerator.playNote(100);
+				break;
+			
+			case KeyEvent.VK_0:
+				toneGenerator.playNote(0);
+				break;
+			
+			case KeyEvent.VK_1:
+				toneGenerator.playNote(1);
+				break;
+			
+			case KeyEvent.VK_2:
+				toneGenerator.playNote(2);
+				break;
+				
+			case KeyEvent.VK_3:
+				toneGenerator.playNote(3);
+				break;
+				
+			case KeyEvent.VK_4:
+				toneGenerator.playNote(4);
+				break;
+				
+			case KeyEvent.VK_5:
+				toneGenerator.playNote(5);
+				break;
+				
+			case KeyEvent.VK_6:
+				toneGenerator.playNote(6);
+				break;
+				
+			case KeyEvent.VK_7:
+				toneGenerator.playNote(7);
+				break;
+				
+			case KeyEvent.VK_8:
+				toneGenerator.playNote(8);
+				break;
+				
+			case KeyEvent.VK_9:
+				toneGenerator.playNote(9);
+				break;
+				
+			case KeyEvent.VK_A:
+				toneGenerator.playNote(10);
+				break;
+				
+			case KeyEvent.VK_B:
+				toneGenerator.playNote(11);
+				break;
+				
+			case KeyEvent.VK_C:
+				toneGenerator.playNote(12);
+				break;
+				
+			case KeyEvent.VK_D:
+				toneGenerator.playNote(13);
+				break;
+				
+			case KeyEvent.VK_E:
+				toneGenerator.playNote(14);
+				break;
+				
+			case KeyEvent.VK_F:
+				toneGenerator.playNote(15);
+				break;
+				
+			case KeyEvent.VK_G:
+				toneGenerator.playNote(16);
+				break;
+				
+			case KeyEvent.VK_H:
+				toneGenerator.playNote(17);
+				break;
+				
+			case KeyEvent.VK_I:
+				toneGenerator.playNote(18);
+				break;
+				
+			case KeyEvent.VK_J:
+				toneGenerator.playNote(19);
+				break;
+				
+			case KeyEvent.VK_K:
+				toneGenerator.playNote(20);
+				break;
+				
+			case KeyEvent.VK_L:
+				toneGenerator.playNote(21);
+				break;
+				
+			case KeyEvent.VK_M:
+				toneGenerator.playNote(22);
+				break;
+				
+			case KeyEvent.VK_N:
+				toneGenerator.playNote(23);
+				break;
+				
+			case KeyEvent.VK_O:
+				toneGenerator.playNote(24);
+				break;
+				
+			case KeyEvent.VK_P:
+				toneGenerator.playNote(25);
+				break;
+				
+			case KeyEvent.VK_Q:
+				toneGenerator.playNote(26);
+				break;
+				
+			case KeyEvent.VK_R:
+				toneGenerator.playNote(27);
+				break;
+				
+			case KeyEvent.VK_S:
 				toneGenerator.playNote(28);
+				break;
+				
+			case KeyEvent.VK_T:
+				toneGenerator.playNote(29);
+				break;
+				
+			case KeyEvent.VK_U:
+				toneGenerator.playNote(30);
+				break;
+				
+			case KeyEvent.VK_V:
+				toneGenerator.playNote(31);
+				break;
+				
+			case KeyEvent.VK_W:
+				toneGenerator.playNote(32);
+				break;
+				
+			case KeyEvent.VK_X:
+				toneGenerator.playNote(33);
+				break;
+				
+			case KeyEvent.VK_Y:
+				toneGenerator.playNote(34);
+				break;
+				
+			case KeyEvent.VK_Z:
+				toneGenerator.playNote(35);
+				break;
+				
+				default:
+					toneGenerator.playNote(60);
+			
 			}
 		}
 
--- src/toneGenerator.java
+++ src/toneGenerator.java
@@ -1,15 +1,7 @@
 import javax.sound.midi.*;
-
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.nio.ByteBuffer;
 import java.util.Scanner;
-
 import javax.sound.sampled.*;
-import javax.swing.JEditorPane;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JTextPane;
 
 /**
  * 
@@ -21,9 +13,9 @@ import javax.swing.JTextPane;
 
 public class toneGenerator implements Runnable{
 	
-	public static int[] notes1 = new int[]{ 60, 62, 64, 65, 67, 69, 71, 72, 72, 71, 69, 67, 65, 64, 62, 60 };
-	public static int[] notes2 = new int[]{ 21, 21, 21,  128,  128, 21,  21,  21, 128, 64,  21,  21,  21, 128, 64, 21, 21,  21, 128 };
-	public static int[] notes3 = new int[]{127,64,127,64,127,64,127,64,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,127,127,64,127,64,127,64,127,64,127,64};
+	//public static int[] notes1 = new int[]{ 60, 62, 64, 65, 67, 69, 71, 72, 72, 71, 69, 67, 65, 64, 62, 60 };
+	//public static int[] notes2 = new int[]{ 21, 21, 21,  128,  128, 21,  21,  21, 128, 64,  21,  21,  21, 128, 64, 21, 21,  21, 128 };
+	//public static int[] notes3 = new int[]{127,64,127,64,127,64,127,64,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,127,127,64,127,64,127,64,127,64,127,64};
 	
 	private static int[] noteArray;
 	private static int singleNote;
@@ -39,35 +31,6 @@ public class toneGenerator implements Runnable{
 	}
 
 	public static void main(String[] args) throws InterruptedException, LineUnavailableException {
-		
-		// AUDIO SAMPLE RATE
-		final int RATE = 44100;
-		
-		// AUDIO SAMPLE SIZE IN BYTES
-		final int SIZE = 2;
-		
-		// FREQUENCY OF THE SINE WAVE IN HZ
-		double frequency = 440;
-		
-		SourceDataLine line;
-
-		//Position through the sine wave as a percentage (i.e. 0 to 1 is 0 to 2*PI)
-		double fCyclePosition = 0;   
-		
-		// ARRAYS FOR NOTES
-		
-		int[] notes = new int[100];
-		for (int i = 0; i < notes.length; i++) {
-			notes[i] = i;
-		}
-		
-		// PROMPT TO PLAY MUSIC OR GO TO KEYBOARD MODE
-		Scanner input = new Scanner(System.in);
-		System.out.println("Press 1 to play music or 2 to go to keybord mode");
-		
-		if (/*input.nextInt() == 2*/ true) {
-			System.out.println("keyboard mode activated.");
-			playNote(70);
 			
 			PianoGui gui = new PianoGui();
 			gui.setTitle("Essay Generator");
@@ -75,76 +38,11 @@ public class toneGenerator implements Runnable{
 			gui.setLocationRelativeTo(null);
 			gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			gui.setVisible(true);
-			
-		} else {
-			System.out.println("Music mode activated.");
-		
-		(new Thread(new toneGenerator(notes1))).start();
-		
-		//Thread.sleep(4000);
-		(new Thread(new toneGenerator(notes2))).start();
-		
-		
-		(new Thread(new toneGenerator(notes3))).start();
-		
-		}
-		
-		// SINGLE TONE EXAMPEL (NOT BEING USED)
 
-		//Open up audio output, using 44100hz sampling rate, 16 bit samples, mono, and big endian byte ordering
-		/*AudioFormat format = new AudioFormat(RATE, 16, 1, true, true);
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-
-		if (!AudioSystem.isLineSupported(info)){
-			System.out.println("Line matching " + info + " is not supported.");
-			throw new LineUnavailableException();
-		}
-
-		line = (SourceDataLine)AudioSystem.getLine(info);
-		line.open(format);  
-		line.start();
-
-		// Make our buffer size match audio system's buffer
-		ByteBuffer cBuf = ByteBuffer.allocate(line.getBufferSize());   
-
-		// Output for roughly 5 seconds
-		int ctSamplesTotal = RATE * 5;
-
-
-		//On each pass main loop fills the available free space in the audio buffer
-		//Main loop creates audio samples for sine wave, runs until we tell the thread to exit
-		//Each sample is spaced 1/SAMPLING_RATE apart in time
-		while (ctSamplesTotal > 0) {
-			double fCycleInc = frequency / RATE;  // Fraction of cycle between samples
-
-			cBuf.clear();     // Discard samples from previous pass
-
-			// Figure out how many samples we can add
-			int ctSamplesThisPass = line.available() / SIZE;   
-			for (int i=0; i < ctSamplesThisPass; i++) {
-				cBuf.putShort((short)(Short.MAX_VALUE * Math.sin(2 * Math.PI * fCyclePosition)));
-
-				fCyclePosition += fCycleInc;
-				if (fCyclePosition > 1)
-					fCyclePosition -= 1;
-			}
-
-			//Write sine samples to the line buffer.  If the audio buffer is full, this will 
-			// block until there is room (we never write more samples than buffer will hold)
-			line.write(cBuf.array(), 0, cBuf.position());            
-			ctSamplesTotal -= ctSamplesThisPass;     // Update total number of samples written 
-
-			//Wait until the buffer is at least half empty  before we add more
-			while (line.getBufferSize() / 2 < line.available())   
-				Thread.sleep(1);                                             
-		}
-
-		//Done playing the whole waveform, now wait until the queued samples finish 
-		//playing, then clean up and exit
-		line.drain();                                         
-		line.close();
-		*/
-
+	}
+	
+	public static void keyboardNote(int note) {
+		(new Thread(new toneGenerator(note))).start();
 	}
 	
 	// THREADED METHOD TO PLAY MIDI TONES
