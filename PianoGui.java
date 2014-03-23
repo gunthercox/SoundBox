73ec736c9f929b9fd7a196100729251bd24f7b3e
Gunther Cox <gunthercx@gmail.com>
2014-03-22 20:22:59 -0400

    removed case select

---
+++ PianoGui.java
@@ -0,0 +1,36 @@
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+
+
+public class PianoGui extends JFrame implements KeyListener {
+	private static final long serialVersionUID = 1L;
+	public PianoGui() {
+
+		setLayout(new BorderLayout());
+
+		JEditorPane text = new JTextPane();
+
+		add(text);
+		text.addKeyListener(this);
+
+		text.requestFocusInWindow();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+	// KEYBOARD EVENTS
+	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {
+	    int key = e.getKeyCode();
+	    toneGenerator.playNote(key);
+	}
+
+}
--- toneGenerator.java
+++ toneGenerator.java
@@ -1,7 +1,12 @@
-import javax.sound.midi.*;
-import java.util.Scanner;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.MidiChannel;
+
+import javax.sound.midi.MidiUnavailableException;
+
 import javax.sound.sampled.*;
-import javax.swing.JFrame;
+
+import java.util.Scanner;
 
 /**
  * 
@@ -11,12 +16,8 @@ import javax.swing.JFrame;
  *
  */
 
-public class toneGenerator implements Runnable{
-	
-	//public static int[] notes1 = new int[]{ 60, 62, 64, 65, 67, 69, 71, 72, 72, 71, 69, 67, 65, 64, 62, 60 };
-	//public static int[] notes2 = new int[]{ 21, 21, 21,  128,  128, 21,  21,  21, 128, 64,  21,  21,  21, 128, 64, 21, 21,  21, 128 };
-	//public static int[] notes3 = new int[]{127,64,127,64,127,64,127,64,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,127,127,64,127,64,127,64,127,64,127,64};
-	
+public class toneGenerator implements Runnable {
+
 	private static int[] noteArray;
 	private static int singleNote;
 	
@@ -36,7 +37,6 @@ public class toneGenerator implements Runnable{
 			gui.setTitle("Essay Generator");
 			gui.setSize(800, 600);
 			gui.setLocationRelativeTo(null);
-			gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			gui.setVisible(true);
 
 	}
